bb46cba7246376748a0b35a73a73814c07aeece8
Nader Zamanzadeh <naderz@connectsis.com>
2018-11-19 15:47:47 -0800

    New Config files - Changed with find_replace

--- find_replace.py
+++ find_replace.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-import os
-import sys
-import fileinput
-
 original_file = open("router_config.TXT", "r+")
 
 read_original = original_file.read()
+
+original_file.close()
 
 changed_file = read_original.replace("Serial0/0/0", "Changed0/0/0")
 
@@ -15,7 +13,6 @@ new_file = open("new_config.txt", "w")
 new_file.write(changed_file)
 
 new_file.close()
-
 
 exit()
 
